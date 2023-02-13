433b47f2060412fa6fc21f7d1ee6c04eb29151f4
MajorX234 <majorx234@googlemail.com>
2023-02-13 23:23:06 +0100

    Add imgae output as png

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,14 @@
+use image::save_buffer_with_format;
+use image::ColorType;
+use image::ImageError;
 use num::complex::Complex;
+use std::fs::File;
 
-fn complex_square_add_loop(c: Complex<f64>, limit: usize) -> Option<usize> {
+fn complex_square_add_loop(c: Complex<f64>, limit: usize) -> Option<u8> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
-            return Some(i);
+            return Some(i as u8);
         }
         z = z * z + c
     }
@@ -18,25 +22,36 @@ fn calculate_mandelbrot(
     y_max: f64,
     resolution_x: usize,
     resolution_y: usize,
-) -> Vec<Vec<usize>> {
-    let mut rows: Vec<Vec<usize>> = Vec::new();
+) -> Vec<u8> {
+    let mut rows: Vec<u8> = vec![0; resolution_y * resolution_x];
     for py in 0..resolution_y {
         let y = (py as f64 / ((resolution_y - 1) as f64)) * (y_max - y_min) + y_min;
-        let mut column: Vec<usize> = Vec::new();
         for px in 0..resolution_x {
             let x = (px as f64 / (resolution_x - 1) as f64) * (x_max - x_min) + x_min;
-            let xy_escape = complex_square_add_loop(Complex::new(x, y), 100);
+            let xy_escape = complex_square_add_loop(Complex::new(x, y), 255);
             match xy_escape {
-                Some(xy_escape) => column.push(xy_escape),
-                None => column.push(0),
+                Some(xy_escape) => rows[py * resolution_x + px] = xy_escape,
+                None => rows[py * resolution_x + px] = 0,
             }
         }
-        rows.push(column);
     }
     return rows;
 }
 
+fn write_png(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), ImageError> {
+    image::save_buffer_with_format(
+        filename,
+        pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::L8,
+        //image::ColorType::Rgb8
+        image::ImageFormat::Png,
+    )?;
+    Ok(())
+}
+
 fn main() {
-    let my_mandelbrot = calculate_mandelbrot(-2.0, 1.0, -1.0, 1.0, 10, 10);
-    println!("{:?}", my_mandelbrot);
+    let my_mandelbrot = calculate_mandelbrot(-1.5, 0.5, -1.0, 1.0, 1000, 1000);
+    write_png("test.png", &my_mandelbrot, (1000, 1000));
 }
