341b713e7a2dd7ad3a44ee2112170f7946021402
MajorX234 <majorx234@googlemail.com>
2023-02-16 23:55:33 +0100

    Add command line parameters

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,23 +9,57 @@ use std::time::SystemTime;
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// resolution in x
-    #[arg(short, long, default_value_t = 1000)]
+    #[arg(long, default_value_t = 1000)]
     pub xres: usize,
 
     /// resolution in y
-    #[arg(short, long, default_value_t = 1000)]
+    #[arg(long, default_value_t = 1000)]
     pub yres: usize,
+
+    /// minimum of x
+    #[arg(long, default_value_t = -1.5)]
+    pub x_min: f64,
+
+    /// maximum of x
+    #[arg(long, default_value_t = 0.5)]
+    pub x_max: f64,
+
+    /// minimum of y
+    #[arg(long, default_value_t = -1.0)]
+    pub y_min: f64,
+
+    /// maximum of y
+    #[arg(long, default_value_t = 1.0)]
+    pub y_max: f64,
+
+    /// number of threads
+    #[arg(short, long, default_value_t = 8)]
+    pub threads: usize,
+
+    /// filepath
+    #[arg(short, long, value_name = "filepath")]
+    pub filepath: Option<String>,
 }
 
 fn main() {
-    let threads = 8;
     let args = Args::parse();
+    let threads = args.threads;
     assert_eq!(args.yres % threads, 0);
+    let filepath = if let Some(filepath) = args.filepath {
+        filepath
+    } else {
+        "test.png".to_string()
+    };
     let time_before = SystemTime::now();
     let mut my_mandelbrot: Vec<u8> = vec![0; args.xres * args.yres];
     let pixels_per_thread = args.xres * args.yres / threads;
-    let y_min = -1.0;
-    let y_max = 1.0;
+    let y_min = args.y_min; //-1.0;
+    let y_max = args.y_max; //1.0;
+    let x_min = args.x_min; //-1.5;
+    let x_max = args.x_max; //0.5;
+    let xres = args.xres;
+    let yres = args.yres;
+
     let delta_y = (y_max - y_min) / threads as f64;
     let y_values = (0..threads)
         .map(|x| x as f64 * delta_y + y_min)
@@ -37,14 +71,12 @@ fn main() {
     crossbeam::scope(|spawner| {
         let wg = crossbeam::sync::WaitGroup::new();
         for (i, (mandelbrot, y_value)) in mandelbrots.into_iter().zip(y_values).enumerate() {
-            let xres = args.xres.clone();
-            let yres = args.yres.clone();
             let wg_clone = wg.clone();
             let thread = spawner.spawn(move |_| {
                 calculate_mandelbrot(
                     mandelbrot,
-                    -1.5,
-                    0.5,
+                    x_min,
+                    x_max,
                     y_value,
                     y_value + delta_y,
                     xres,
@@ -59,5 +91,5 @@ fn main() {
     let time_after = SystemTime::now();
     let time_difference = time_after.duration_since(time_before).unwrap().as_millis();
     print!("caclulation take {} ms", time_difference);
-    write_png("test.png", &my_mandelbrot, (args.xres, args.yres)).unwrap();
+    write_png(&filepath, &my_mandelbrot, (args.xres, args.yres)).unwrap();
 }
